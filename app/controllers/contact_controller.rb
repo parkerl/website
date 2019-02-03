324d362d66ff24e7ece860293979c99a3656b40d
Andrew Courter <andrew@earthvectors.com>
2019-02-03 16:34:31 -0600

    Make sure all params are there before sending emails

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -2,8 +2,10 @@ class ContactController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def create
-    ContactMailer.contact_email(params).deliver
-    ContactMailer.thank_you_email(params[:name], params[:email]).deliver
+    if params.include?(:name) && params.include?(:email) && params.include?(:message)
+      ContactMailer.contact_email(params).deliver
+      ContactMailer.thank_you_email(params[:name], params[:email]).deliver
+    end
 
     head :ok, content_type: "text/html"
   end
