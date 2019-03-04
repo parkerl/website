0d038e1aa3fc27b1275aa04e1e6da9c721478595
Andrew Courter <exosyphon@yahoo.com>
2019-03-04 01:20:48 -0600

    Require CSRF token and verify recaptcha in controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,9 @@
 class ApplicationController < ActionController::Base
-  skip_before_action :verify_authenticity_token
-
   def index
   end
 
   def create
-    if params.include?(:name) && params.include?(:email) && params.include?(:message)
+    if verify_recaptcha && params.include?(:name) && params.include?(:email) && params.include?(:message)
       ContactMailer.contact_email(params[:name], params[:email], params[:message]).deliver
       ContactMailer.thank_you_email(params[:name], params[:email]).deliver
     end
