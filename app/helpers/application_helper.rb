3386a5ea72867f2ba8649373b698d29c35a30ef9
Mihir Parikh <mihir@earthvectors.com>
2019-05-24 15:35:37 -0600

    Add RSS feed for EarthVectors medium publication

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+  require "rss"
+  require "open-uri"
+
+  def blog_posts
+    rss = RSS::Parser.parse(open("https://medium.com/feed/earthvectors").read, false).items
+    rss.map do |post|
+      {
+        title: post.title,
+        date: post.pubDate,
+        link: post.link,
+        description: post.description,
+        creator: post.dc_creator,
+        content: post.content_encoded.html_safe,
+      }
+    end
+  end
 end
