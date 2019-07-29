f21960fb6bc0892917adaf4be09e5bfe6d31b682
Mihir Parikh <mihir@earthvectors.com>
2019-07-29 10:18:14 -0700

    Link Blogs to EVI Medium page
    -Remove blogs section

--- app/service/feed_reader.rb
+++
@@ -1,9 +0,0 @@
-require 'rss'
-require 'open-uri'
-
-class FeedReader
-  def items
-      io = Kernel.open(Figaro.env.FEED_URI).read
-      RSS::Parser.parse(io, false).items.sort_by(&:pubDate).reverse
-  end
-end
